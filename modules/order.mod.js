f79b89d4f08a1572187b799918b3d5c25b52eab3
Hunter Parcells <hparcells@icloud.com>
2018-08-28 12:57:08 -0400

    Sends embed saying order has been created.

--- modules/order.mod.js
+++ modules/order.mod.js
@@ -1,7 +1,26 @@
 exports.id = 'order';
 
 exports.onLoad = api => {
+    let regex = new RegExp("p!order (.*)");
+    
     api.commands.add('order', (msg) => {
-        // TODO
-    })
+        // Get Order. The order is order[1].
+        order = regex.exec(msg.content);
+
+        // Sends an embed.
+        msg.channel.send({embed: {
+            color: "3447003",
+            title: "Ticket Created",
+            description: "Your order has been placed!",
+            fields: [{
+                name: "Your Ticket ID",
+                value: "`EXAMPLE`"
+              }
+            ],
+            timestamp: new Date(),
+        }});
+
+        // Sends Ticket
+        // api.guilds.get(483736796354576394).channels.get(483743363909025806).send("Hello")
+    });
 };
--- modules/ping.mod.js
+++ modules/ping.mod.js
@@ -3,5 +3,6 @@ exports.id = 'ping';
 exports.onLoad = api => {
     api.commands.add('ping', (msg) => {
         msg.channel.send(':ping_pong: Pong!');
+        console.log();
     })
 };
