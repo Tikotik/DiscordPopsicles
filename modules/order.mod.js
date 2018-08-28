581ed46dc83d4432f9b46818fe5aafb99fa9b95b
Hunter Parcells <hparcells@icloud.com>
2018-08-28 14:25:15 -0400

    reports to tickets channel

--- modules/order.mod.js
+++ modules/order.mod.js
@@ -15,12 +15,38 @@ exports.onLoad = api => {
             fields: [{
                 name: "Your Ticket ID",
                 value: "`EXAMPLE`"
-              }
-            ],
+              }],
             timestamp: new Date(),
         }});
 
         // Sends Ticket
-        // api.guilds.get(483736796354576394).channels.get(483743363909025806).send("Hello")
+        api.client.guilds.get("483736796354576394").channels.get("483743363909025806").send({embed: {
+            color: "3447003",
+            title: msg.author.username,
+            fields: [{
+                name: ":ticket: New Ticket",
+                value: msg.author.username + " would like a popsicle.",
+            },
+            {
+                name: ":newspaper2: Popsicle Description",
+                value: order[1],
+            },
+            {
+                name: ":hash: Ticket ID",
+                value: "EXAMPLE",
+            },
+            {
+                name: ":computer: Guild Infomation",
+                value: "This ticket came from " + msg.guild + " (" + msg.guild.id+ ")" + " in " + msg.channel + " (" + msg.channel.id + ").",
+            },
+            {
+                name: ":white_check_mark: Ticket Status",
+                value: "Unclaimed", 
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: "Discord Popsicles"
+            }
+        }});
     });
 };
