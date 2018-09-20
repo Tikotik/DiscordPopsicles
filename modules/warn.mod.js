785f35c96709c95182ac4d619e2c5d28df0becbf
Hunter Parcells <hparcells@icloud.com>
2018-09-20 15:18:22 -0400

    Added Warn Command

---
+++ modules/warn.mod.js
@@ -0,0 +1,47 @@
+const colors = require("colors");
+const fsn = require("fs-nextra");
+
+exports.id = "warn";
+
+exports.onLoad = api => {
+    api.commands.add("warn", (msg) => {
+        let content = msg.content.substring(7);
+        let server = content.substring(0, 18);
+        let warning = content.substring(19);
+
+        fsn.readJSON("./warnings.json").then((warningDB) => {
+            let entry = warningDB[server];
+
+            if(entry === undefined) {
+                let totalWarnings = 1;
+
+                // Set JSON information.
+                warningDB[server] = {
+                    warnings: totalWarnings
+                }
+                
+                // Write JSON information.
+                fsn.writeJSON("./warnings.json", warningDB, {
+                    replacer: null,
+                    spaces: 4
+                });
+            }else {
+                let totalWarnings = entry.warnings + 1;
+
+                // Set JSON information.
+                warningDB[server] = {
+                    warnings: totalWarnings
+                }
+
+                // Write JSON information.
+                fsn.writeJSON("./warnings.json", warningDB, {
+                    replacer: null,
+                    spaces: 4
+                });
+            }
+        });
+        
+        // Logs in console.
+        console.log(colors.green(`${msg.author.username} used the warn command.`));
+    })
+};
