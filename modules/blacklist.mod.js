6746df9101e4754c997dc288bc119dbdcd0b14cb
Hunter Parcells <hparcells@icloud.com>
2018-09-21 15:15:32 -0400

    Fixed Blacklist

--- modules/blacklist.mod.js
+++ modules/blacklist.mod.js
@@ -8,28 +8,36 @@ exports.onLoad = api => {
         let content = msg.content.substring(12);
         let server = content.substring(0, 18);
 
-        fsn.readJSON("./blacklist.json").then((blacklistDB) => {
-            let entry = blacklistDB[server];
-
-            if(entry === undefined) {
-                // Set JSON information.
-                blacklistDB[server] = {
-                    blacklisted: true
+        // Checks if correct user is using this command.
+        if(msg.author.id === 481810179218997266) {
+            fsn.readJSON("./blacklist.json").then((blacklistDB) => {
+                let entry = blacklistDB[server];
+    
+                if(entry === undefined) {
+                    // Set JSON information.
+                    blacklistDB[server] = {
+                        blacklisted: true
+                    }
+                    
+                    // Write JSON information.
+                    fsn.writeJSON("./blacklist.json", blacklistDB, {
+                        replacer: null,
+                        spaces: 4
+                    });
+    
+                    msg.channel.send(`You have blacklisted ${api.client.guilds.get(server).name}!`);
+                }else {
+                    msg.channel.send("This server is already blacklisted.");
                 }
-                
-                // Write JSON information.
-                fsn.writeJSON("./blacklist.json", blacklistDB, {
-                    replacer: null,
-                    spaces: 4
-                });
+            });
+            
+            // Logs in console.
+            console.log(colors.green(`${msg.author.username} used the blacklist command.`));
+        }else {
+            msg.reply("You do not have permission to run this command.");
 
-                msg.channel.send(`You have blacklisted ${api.client.guilds.get(server).name}!`);
-            }else {
-                msg.channel.send("This server is already blacklisted.");
-            }
-        });
-        
-        // Logs in console.
-        console.log(colors.green(`${msg.author.username} used the blacklist command.`));
+            // Logs in console.
+            console.log(colors.red(`${msg.author.username} did not have permission to run the blacklist command.`));
+        }
     })
 };
--- modules/order.mod.js
+++ modules/order.mod.js
@@ -11,95 +11,90 @@ exports.onLoad = api => {
         fsn.readJSON("./blacklist.json").then((blacklistDB) => {
             let entry = blacklistDB[msg.guild.id];
             
-            // Checks if correct user is using this command.
-            if(msg.author.id === 481810179218997266) {
-                // Checks is server is blacklisted or not.
-                if(entry === undefined) {
-                    // Gets ticket ID.
-                    function generateID() {
-                        let ticketGen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
-                        let ticketStr = "";
-            
-                        for(let i = 0; i < 7; i++) {
-                            ticketStr += ticketGen[Math.floor(Math.random() * ticketGen.length)];
-                        }
-            
-                        return ticketStr;
+            // Checks is server is blacklisted or not.
+            if(entry === undefined) {
+                // Gets ticket ID.
+                function generateID() {
+                    let ticketGen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+                    let ticketStr = "";
+        
+                    for(let i = 0; i < 7; i++) {
+                        ticketStr += ticketGen[Math.floor(Math.random() * ticketGen.length)];
                     }
-                    const ticketID = generateID();
-            
-                    // Sends ticket information to tickets channel.
-                    api.client.guilds.get("483736796354576394").channels.get("483743363909025806").send({embed: {
-                        color: 0xFFFFFF,
-                        title: msg.author.username,
-                        fields: [{
-                            name: ":ticket: New Ticket",
-                            value: `${msg.author.username} would like a popsicle.`,
-                        }, {
-                            name: ":newspaper2: Popsicle Description",
-                            value: order,
-                        }, {
-                            name: ":hash: Ticket ID",
-                            value: ticketID,
-                        }, {
-                            name: ":computer: Guild Infomation",
-                            value: `This ticket came from ${msg.guild} (${msg.guild.id}) in ${msg.channel} (${msg.channel.id}).`,
-                        }, {
-                            name: ":white_check_mark: Ticket Status",
-                            value: "Unclaimed", 
-                        }],
-                        timestamp: new Date(),
-                        footer: {
-                            text: "Discord Popsicles"
+        
+                    return ticketStr;
+                }
+                const ticketID = generateID();
+        
+                // Sends ticket information to tickets channel.
+                api.client.guilds.get("483736796354576394").channels.get("483743363909025806").send({embed: {
+                    color: 0xFFFFFF,
+                    title: msg.author.username,
+                    fields: [{
+                        name: ":ticket: New Ticket",
+                        value: `${msg.author.username} would like a popsicle.`,
+                    }, {
+                        name: ":newspaper2: Popsicle Description",
+                        value: order,
+                    }, {
+                        name: ":hash: Ticket ID",
+                        value: ticketID,
+                    }, {
+                        name: ":computer: Guild Infomation",
+                        value: `This ticket came from ${msg.guild} (${msg.guild.id}) in ${msg.channel} (${msg.channel.id}).`,
+                    }, {
+                        name: ":white_check_mark: Ticket Status",
+                        value: "Unclaimed", 
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                        text: "Discord Popsicles"
+                    }
+                }}).then((m) => {
+                    m = m.id;
+        
+                    // Sets ticket info.
+                    fsn.readJSON("./orders.json").then(orderDB => {
+                        // Set JSON information.
+                        if (!orderDB[ticketID]) orderDB[ticketID] = {
+                            "orderID": ticketID,
+                            "userID": msg.author.id,
+                            "guildID": msg.guild.id,
+                            "channelID": msg.channel.id,
+                            "order": order,
+                            "status": "Unclaimed",
+                            "ticketChannelMessageID": m
                         }
-                    }}).then((m) => {
-                        m = m.id;
             
-                        // Sets ticket info.
-                        fsn.readJSON("./orders.json").then(orderDB => {
-                            // Set JSON information.
-                            if (!orderDB[ticketID]) orderDB[ticketID] = {
-                                "orderID": ticketID,
-                                "userID": msg.author.id,
-                                "guildID": msg.guild.id,
-                                "channelID": msg.channel.id,
-                                "order": order,
-                                "status": "Unclaimed",
-                                "ticketChannelMessageID": m
+                        // Write JSON information.
+                        fsn.writeJSON("./orders.json", orderDB, {
+                            replacer: null,
+                            spaces: 4
+                        }).then(() => {
+                            // Sends an embed to the customer.
+                            msg.channel.send({embed: {
+                                color: 0xFFFFFF,
+                                title: "Ticket Created",
+                                description: "Your order has been placed!",
+                                fields: [{
+                                    name: "Your Ticket ID",
+                                    value: `\`${ticketID}\``
+                                    }],
+                                timestamp: new Date(),
+                            }});
+            
+                            // Logs in console.
+                            console.log(colors.green(`${msg.author.username} ordered "${order}" in ${msg.guild.name} (${msg.guild.id}) in ${msg.channel.name} (${msg.channel.id}).`));
+                        }).catch((err) => {
+                            if(err) {
+                                msg.reply(`There was a database error! Show the following message to a developer: \`\`\`${err}\`\`\``)
+                                console.log(colors.red(`Error in order ${ticketID}: ${err}`));
                             }
-                
-                            // Write JSON information.
-                            fsn.writeJSON("./orders.json", orderDB, {
-                                replacer: null,
-                                spaces: 4
-                            }).then(() => {
-                                // Sends an embed to the customer.
-                                msg.channel.send({embed: {
-                                    color: 0xFFFFFF,
-                                    title: "Ticket Created",
-                                    description: "Your order has been placed!",
-                                    fields: [{
-                                        name: "Your Ticket ID",
-                                        value: `\`${ticketID}\``
-                                        }],
-                                    timestamp: new Date(),
-                                }});
-                
-                                // Logs in console.
-                                console.log(colors.green(`${msg.author.username} ordered "${order}" in ${msg.guild.name} (${msg.guild.id}) in ${msg.channel.name} (${msg.channel.id}).`));
-                            }).catch((err) => {
-                                if(err) {
-                                    msg.reply(`There was a database error! Show the following message to a developer: \`\`\`${err}\`\`\``)
-                                    console.log(colors.red(`Error in order ${ticketID}: ${err}`));
-                                }
-                            });
                         });
                     });
-                }else {
-                    msg.reply("This server is currently blacklisted. If you wish to continue using this bot, please join our support server and discuss it there. You can get the invite link by doing `p!invite`.");
-                }
+                });
             }else {
-                msg.reply("You do not have permission to run this command.");
+                msg.reply("This server is currently blacklisted. If you wish to continue using this bot, please join our support server and discuss it there. You can get the invite link by doing `p!invite`.");
             }
         });
     });
